8b78f088afe1c76de3211447f3001f1e748dbbe2
Matt Miermans <m.miermans@gmail.com>
2022-07-04 00:13:03 -0700

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ authors_email_str = ', '.join(authors_email_list)
 
 setup(
     name='aio-snowplow-tracker',
-    version='1.0.0a4',
+    version='1.0.0',
     author=authors_str,
     author_email=authors_email_str,
     packages=[
